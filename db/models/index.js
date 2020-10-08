3ae4b9112e72ea8b0930704caea21a0a1c6680ca
Waleed Ali <waleedali070@gmail.com>
2020-10-08 11:05:48 +0500

    fixed sequelize models import issue with webpack

--- db/models/index.js
+++ db/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 /* Custom handler for reading current working directory */
 const models = process.cwd() + '/db/models/' || __dirname;
@@ -23,7 +23,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(models, file))(sequelize, Sequelize.DataTypes);
+    const model = require(`./${file}`)(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
